cd5b944a903e9f05a18af958be0d37f9047f8b2f
Aivanii <aivanellok@gmail.com>
2025-02-20 15:39:02 +0700

    changed project theme

--- app/App.tsx
+++ app/App.tsx
@@ -3,18 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./styles/styles.scss";
 
-//import pages from "pages"
-import HomePage from "./pages/HomePage/Home.page";
 import Error404Page from "./pages/Error404.page";
-//
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/tovari" element={<> </>} />
-      <Route path="/o-nas" element={<> </>} />
-      <Route path="contacti" element={<> </>} />
       <Route path="*" element={<Error404Page />} />
     </Routes>
   </Router>
