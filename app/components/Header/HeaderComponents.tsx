d7dde2345b1809ea694f4dba0341976209c97479
Aivanii <aivanellok@gmail.com>
2025-02-20 16:39:43 +0700

    updated header

--- app/components/Header/HeaderComponents.tsx
+++ app/components/Header/HeaderComponents.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import "./HeaderStyles.scss";
 
-const Header = () => {}
+const Header = () => {
+  return (
+    <header>
+      <div className="LogoHolder">
+        <div className="logoLetter">
+          <span>С</span>
+        </div>
+        <span className="separator"></span>
+        <h1 className="logoName">Сакура</h1>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- app/pages/HomePage/Home.page.tsx
+++ app/pages/HomePage/Home.page.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Header from "../../components/Header/HeaderComponents";
 
 const HomePage = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <h2>Welcome to Sakura</h2>
+      <p>This is the home page.</p>
+      <video className="video-player" controls>
+        <source src="https://uploads-ssl.webflow.com/570118c96d89fd540a96498d/5713d0ada39927f6421c92f2_Fountain-In-Park-transcode.mp4" type="video/mp4" />
+        Ваш браузер не поддерживает видео.
+      </video>
+    </div>
+  );
+};
 
-export default HomePage; 
+export default HomePage;
