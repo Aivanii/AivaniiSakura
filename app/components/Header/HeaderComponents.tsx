5381ed19626393379a475465ec3c5c3e893a57ce
Aivanii <aivanellok@gmail.com>
2025-02-21 13:42:24 +0700

    updated header

--- app/components/Header/HeaderComponents.tsx
+++ app/components/Header/HeaderComponents.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./HeaderStyles.scss";
-
+import { Container } from "@mui/material";
+import { Link } from "@mui/icons-material";
 const Header = () => {
   return (
     <header>
-      <div className="LogoHolder">
-        <div className="logoLetter">
-          <span>С</span>
+      <Container>
+        <div className="LogoHolder">
+          <div className="logoLetter">
+            <span>С</span>
+          </div>
+          <h1 className="logoName">Сакура</h1>
+          <span className="verticalSeparator"></span>
         </div>
-        <span className="separator"></span>
-        <h1 className="logoName">Сакура</h1>
-      </div>
+        <Container maxWidth="md">
+          <h3>
+            Студия ландшафтного дизайна в НСК
+          </h3>
+        </Container>
+      </Container>
     </header>
   );
 };
--- app/pages/HomePage/Home.page.tsx
+++ app/pages/HomePage/Home.page.tsx
@@ -1,16 +1,36 @@
 import React from "react";
 import Header from "../../components/Header/HeaderComponents";
-
+import { Container } from "@mui/material";
+import "./MainPageStyles.scss"
 const HomePage = () => {
   return (
     <div>
       <Header />
-      <h2>Welcome to Sakura</h2>
-      <p>This is the home page.</p>
-      <video className="video-player" controls>
-        <source src="https://uploads-ssl.webflow.com/570118c96d89fd540a96498d/5713d0ada39927f6421c92f2_Fountain-In-Park-transcode.mp4" type="video/mp4" />
-        Ваш браузер не поддерживает видео.
-      </video>
+      <div>
+        <video
+          className="video-player"
+          autoPlay={true}
+          disablePictureInPicture={true}
+          disableRemotePlayback={true}
+          loop={true}
+          muted={true}
+        >
+          <source
+            src="https://uploads-ssl.webflow.com/570118c96d89fd540a96498d/5713d0ada39927f6421c92f2_Fountain-In-Park-transcode.mp4"
+            type="video/mp4"
+          />
+          Ваш браузер не поддерживает видео.
+        </video>
+        <Container className="subTextOnVideo" maxWidth="lg">
+          <h1>
+            Проектирование 
+            <span> садового дизайна</span> и <span>благоустройство </span>
+            территории
+          </h1>
+          <div className="horizontalSeparator"></div>
+          <p>Только до 30 февраля вызов специалиста совершенно <span>бесплатно</span>.</p>
+        </Container>
+      </div>
     </div>
   );
 };
