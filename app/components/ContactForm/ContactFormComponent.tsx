476cc5e012a50f1a4ca7cc2a1cad44f763bbbdc5
Aivanii <aivanellok@gmail.com>
2025-02-24 15:38:14 +0700

    added contact form

---
+++ app/components/ContactForm/ContactFormComponent.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from "react";
+import "./ContactFormStyles.scss";
+import { Container } from "@mui/material";
+import AOS from "aos";
+import "aos/dist/aos.css";
+const ContactForm = () => {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  }, []);
+  return (
+    <div className="contactFormContainer">
+      <img
+        src="https://uploads-ssl.webflow.com/570118c96d89fd540a96498d/5713f13cfbe891be774e9521_Pond%20Form%20Bg.jpg"
+        alt="contact form bg"
+      ></img>
+      <Container maxWidth="md" className="innerContainerContainerForm">
+        <h2 data-aos="fade-down" data-aos-delay="300">
+          Сохраните свои <span className="pinkTextSpan">нервы </span>и{" "}
+          <span className="pinkTextSpan">время</span>, Запишитесь прямо сейчас!
+        </h2>
+        <div
+          className="horizontalSeparatorVer2"
+          data-aos="fade-down"
+          data-aos-delay="500"
+        ></div>
+        <Container
+          maxWidth="sm"
+          className="formHolder"
+          data-aos="fade-down"
+          data-aos-delay="800"
+        >
+          <h3>
+            Закажите услуги <span className="pinkTextSpan">Сакуры</span> со
+            скидкой
+            <span className="pinkTextSpan"> 10%</span> только сегодня!
+          </h3>
+          <form>
+            <div>
+              <label htmlFor="name">Ваше имя:</label>
+              <input
+                type="text"
+                placeholder="Введите ваше имя"
+                name="name"
+                required={true}
+              />
+            </div>
+            <div>
+              <label htmlFor="phone">Ваш телефон:</label>
+              <input
+                type="tel"
+                placeholder="Введите ваш телефон"
+                name="phone"
+                required={true}
+              />
+            </div>
+            <div>
+              <label htmlFor="service">Интересующая вас услуга:</label>
+              <select name="service" required={true}>
+                <option value="">Выберите услугу...</option>
+                <option value="Пруды">Пруды</option>
+                <option value="Фонтаны">Фонтаны</option>
+                <option value="Водоёмы">Водоёмы</option>
+                <option value="Обслуживание">Обслуживание</option>
+              </select>
+            </div>
+            <button type="submit">Заказать проект</button>
+          </form>
+        </Container>
+      </Container>
+    </div>
+  );
+};
+
+export default ContactForm;
